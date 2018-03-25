e61732f092f6916005c416f49a7df11262db0e25
brumraul <brumraul@gmail.com>
2018-03-25 13:28:28 -0300

    Correccion de .h

--- tp1/src/IOUtils.h
+++ tp1/src/IOUtils.h
@@ -1,3 +1,5 @@
+#ifndef IOUTILS_H_
+#define IOUTILS_H_
 #include "Matriz.h"
 
 class IOUtils {
@@ -7,4 +9,4 @@ public:
 	Matriz leerEntrada(const std::string &nombreArchivo);		
 	void escribirSalida(const std::string &nombreArchivo, double p, const std::vector<double> rank);
 }; 
-
+#endif
