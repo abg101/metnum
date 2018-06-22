ae1cd56c3a962c2f425d2c600b6f571c586f9276
Nicolas Hernandez <nicoh22@hotmail.com>
2018-06-22 12:45:47 +0000

    implementacion recta

---
+++ tp3/src/Recta.hpp
@@ -0,0 +1,18 @@
+class Recta
+{
+	Recta(int x1, int y1, int x2, int y2);
+	int evaluar(int x);
+	private:
+	int coeficiente;
+	int independiente;
+}
+
+Recta::Recta(int x1, int y1, int x2, int y2)
+{
+	coeficiente = (y1 - y2)/(x1 - x2);
+	independiente = y1 - (coeficiente*x1);
+}
+int Recta::evaluar(int x)
+{
+	return (coeficiente*x)+independiente;
+}
