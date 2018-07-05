40c1550f869292854f937dc4445f6f67ee627a96
Nicolas <nicoh22@hotmail.com>
2018-07-05 20:58:59 -0300

    avance hasta ahora

--- tp3/src/cuadradosMinimos.hpp
+++ tp3/src/cuadradosMinimos.hpp
@@ -4,6 +4,8 @@ Matriz<double> resolverEN(Matriz<double> A, Matriz<double> b)
 {
 	//assert(A.rango() < A.columnas()); //como es SDP es inversible 
 	Matriz<double> At = A.traspuesta();
-	Matriz<double> x = resolver_gauss(At*(A),At*(b));
+	Matriz<double> Ata = At*(A);
+	Matriz<double> Atb = At*(b);
+	Matriz<double> x = resolver_gauss(Ata, Atb);
 	return x;
 }
